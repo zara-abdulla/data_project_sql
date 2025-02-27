c985d810c745cd8d4bee9ffd5deaa5c0651d849c
zara-abdulla <zaraabdulla95@gmail.com>
2025-02-27 14:25:29 +0300

    update

--- project_sql/5_optimal_skills.sql
+++ project_sql/5_optimal_skills.sql
@@ -1,24 +1,38 @@
 
 WITH demand_skills AS (
-        select skill_id,skills, count(sjd.job_id) as demand_count 
+    select skills_dim.skill_id, 
+    skills_dim.skills, 
+    count(skills_job_dim.job_id) as demand_count 
     from job_postings_fact as jpf
-    inner join skills_job_dim as sjd ON sjd.job_id=jpf.job_id
-    INNER JOIN skills_dim as sd ON sd.skill_id=sjd.skill_id
+    inner join skills_job_dim ON skills_job_dim.job_id=jpf.job_id
+    INNER JOIN skills_dim ON skills_dim.skill_id=skills_job_dim.skill_id
     where jpf.job_title_short= 'Data Analyst'
-    and job_work_from_home= TRUE
-    GROUP BY skill_id
+    and job_work_from_home= TRUE  
+    AND salary_year_avg is NOT NULL
+    GROUP BY skills_dim.skill_id
 )
 
-WITH average_salary as (
-        select skill_id,skills, round(avg(salary_year_avg), 0) as avg_salary
+, average_salary as (
+    select skills_job_dim.skill_id,
+    round(avg(jpf.salary_year_avg), 0) as avg_sal
     from job_postings_fact as jpf
-    inner join skills_job_dim as sjd ON sjd.job_id=jpf.job_id
-    INNER JOIN skills_dim as sd ON sd.skill_id=sjd.skill_id
+    inner join skills_job_dim ON skills_job_dim.job_id=jpf.job_id
+    INNER JOIN skills_dim ON skills_dim.skill_id=skills_job_dim.skill_id
     where jpf.job_title_short= 'Data Analyst'
     AND salary_year_avg is NOT NULL
     and job_work_from_home= TRUE
-    GROUP BY skill_id
+    GROUP BY skills_job_dim.skill_id
 )
+
+SELECT demand_skills.skill_id,
+    demand_skills.skills as skill_name,
+    demand_count, avg_sal
+    from demand_skills
+    inner join average_salary on 
+    average_salary.skill_id=demand_skills.skill_id
+    ORDER BY demand_count DESC, avg_sal DESC
+    LIMIT 25;
+
 
 
 
@@ -29,3 +43,19 @@ Answer: What are the most optimal skills to learn (aka it's in high demand and a
 v - Why? Targets skills that offer job security (high demand) and financial benefits (high salaries),
  offering strategic insights for career development in data analysis
 */
+
+
+SELECT 
+    skills_dim.skill_id, 
+    skills_dim.skills AS skill_name, 
+    COUNT(skills_job_dim.job_id) AS demand_count, 
+    ROUND(AVG(jpf.salary_year_avg), 0) AS avg_sal
+FROM job_postings_fact AS jpf
+INNER JOIN skills_job_dim ON skills_job_dim.job_id = jpf.job_id
+INNER JOIN skills_dim ON skills_dim.skill_id = skills_job_dim.skill_id
+WHERE jpf.job_title_short = 'Data Analyst'
+    AND jpf.job_work_from_home = TRUE  
+    AND jpf.salary_year_avg IS NOT NULL
+GROUP BY skills_dim.skill_id
+ORDER BY demand_count DESC, avg_sal DESC
+LIMIT 25;
